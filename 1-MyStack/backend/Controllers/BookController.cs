1a4ff0c206a44d69b1bd0274a263e05599a19398
Pepinelli <lpepinelli@hotmail.com>
2021-12-02 09:00:54 -0300

    folder structure

---
+++ 1-MyStack/backend/Controllers/BookController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+        // GET: api/Book
+        [HttpGet]
+        public ActionResult<List<Models.Book>> GetBooks()
+        {
+            Control.BookControl objBook = new Control.BookControl();
+            return objBook.ListBooks();
+        }
+
+        // GET: api/Book/5
+        [HttpGet("{id}")]
+        public ActionResult<Models.Book> GetBook(int id)
+        {
+            Control.BookControl objBook = new Control.BookControl();
+            var Book = objBook.Read(id);
+
+            if (Book == null)
+                return NotFound();
+
+            return Book;
+        }
+
+        // PUT: api/Book/5
+        [HttpPut("{id}")]
+        public IActionResult PutBook(int id, Models.Book Book)
+        {
+            Control.BookControl objBook = new Control.BookControl();
+
+            if (id != Book.id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                objBook.Update(Book);
+            }
+            catch (Exception e)
+            {
+                if (objBook.Read(id) == null)
+                    return NotFound();
+                else
+                    throw;
+            }
+            return NoContent();
+        }
+
+        // POST: api/Book
+        [HttpPost]
+        public ActionResult<Models.Book> PostBook(Models.Book Book)
+        {
+            Control.BookControl objBook = new Control.BookControl();
+            var cod = objBook.Create(Book);
+
+            return CreatedAtAction(nameof(GetBook), new { id = cod }, Book);
+        }
+
+        // DELETE: api/Book/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            Control.BookControl objBook = new Control.BookControl();
+            var Book = objBook.Read(id);
+            string msg = null;
+            bool result;
+            if (Book == null)
+                return NotFound();
+
+            result = objBook.Delete(id, out msg);
+
+            return Ok(new {result, msg});
+        }
+    }
+}
---
+++ 1-MyStack/backend/DAL/MySQLPersistence.cs
@@ -0,0 +1,159 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace backend.DAL
+{
+    public class MySQLPersistence
+    {
+        MySqlConnection _connection = null;
+        MySqlCommand _cmd = null;
+        bool _closeConnection = true;
+        bool _withTransaction = false;
+        MySqlTransaction _transaction = null;
+
+        Int64 _lastId;
+
+        public long LastId { get => _lastId; set => _lastId = value; }
+
+        public MySQLPersistence(bool closeConnection = true, bool withTransaction = false)
+        {
+           string strCon = @"server=localhost;user=root;database=1.stack;port=3306;password=153624";
+
+            _connection = new MySqlConnection(strCon);
+
+            _closeConnection = closeConnection;
+            _withTransaction = withTransaction;
+            _cmd = _connection.CreateCommand();
+        }
+
+        public void Open()
+        {
+            if (_connection.State != System.Data.ConnectionState.Open)
+            {
+                _connection.Open();
+
+                if (_withTransaction)
+                {
+                    _transaction = _connection.BeginTransaction();
+                }
+            }
+        }
+
+        public void Close()
+        {
+            _connection.Close();
+        }
+
+        public void Commit()
+        {
+
+            _transaction.Commit();
+            _transaction = null;
+        }
+
+        public void Rollback()
+        {
+            _transaction.Rollback();
+            _transaction = null;
+        }
+
+        public DataTable GetData(string select, Dictionary<string, object> parameters = null, bool procedure = false)
+        {
+            if(_withTransaction)
+                _cmd = new MySqlCommand(select, _connection, _transaction);
+            else
+                _cmd.CommandText = select;
+
+            if (procedure)
+            {
+                _cmd.CommandType = CommandType.StoredProcedure;
+            }
+
+            if (parameters != null)
+            {
+                _cmd.Parameters.Clear();
+                foreach (var item in parameters)
+                {
+                    _cmd.Parameters.AddWithValue(item.Key, item.Value);
+                }
+            }
+
+            DataTable dt = new DataTable();
+
+            dt.Load(_cmd.ExecuteReader());
+
+            if (_closeConnection)
+                Close();
+
+            return dt;
+        }
+
+
+        public int Execute(string instruction, Dictionary<string, object> parameters = null)
+        {
+            _cmd = new MySqlCommand(instruction, _connection, _transaction);
+
+            if (parameters != null)
+            {
+                _cmd.Parameters.Clear();
+                foreach (var item in parameters)
+                {
+                    if(item.Value != null)
+                        _cmd.Parameters.AddWithValue(item.Key, item.Value);
+                    else
+                        _cmd.Parameters.AddWithValue(item.Key, DBNull.Value);
+                }
+            }
+
+            _lastId = Convert.ToInt64(_cmd.ExecuteScalar());
+
+            if (_closeConnection)
+                Close();
+
+            return (Int32)_lastId;
+        }
+        public string SqlExceptionHandle(Exception e)
+        {
+            //Dictionary<string, string> Messages = new Dictionary<string, string> {{"A instrução (?<Instruction>.*) conflitou com a restrição do REFERENCE \"(.*)\". O conflito ocorreu no banco de dados \"(.*)\", tabela \"(?<Table>.*)\", column '(.*)'.\r\nA instrução foi finalizada.", "Erro ao {0} o registro, ele possui vínculo com a tabela de {1}."}};
+            Dictionary<string, string> Messages = new Dictionary<string, string> {{"Cannot delete or update a parent row: a foreign key constraint fails (`1.stack`.`(?<Table>.*)`, CONSTRAINT `(.*)` FOREIGN KEY (`(.*)`) REFERENCES `(.*)` (`(.*)`))", "Erro ao excluir ou alterar o registro, ele possui vínculo com a tabela de {1}."}};
+            //Dictionary<string, string> Instructions = new Dictionary<string, string> { { "INSERT", "Inserir" }, { "UPDATE", "Alterar" }, { "DELETE", "Deletar" } };
+            Dictionary<string, string> Tables = new Dictionary<string, string> {
+                { "book", "Livros" },
+                { "genre", "Gêneros" }
+             };
+
+            try{
+                foreach(var reg in Messages.Keys)
+                {
+                    var match = Regex.Match(e.Message, reg);
+                    if (match.Success)
+                    {
+                        /*string friendlyInstruction = "";
+                        if (match.Groups["Instruction"] != null)
+                        {
+                            friendlyInstruction = Instructions[match.Groups["Instruction"].Value] ??
+                                                match.Groups["Instruction"].Value;
+                        }*/
+                        string friendlyTable = "";
+                        if (match.Groups["Table"] != null)
+                        {
+                            friendlyTable = Tables[match.Groups["Table"].Value] ??
+                                                match.Groups["Table"].Value;
+                        }
+                        var groups = match.Groups.Cast<Group>().Select(x => x.Value);
+                        var strings = new [] {friendlyTable};
+                        return String.Format(Messages[reg], strings.Concat(groups).ToArray());
+                    }
+                }
+                return "Erro inesperado de banco de dados.";
+            }
+            catch{
+                return "Erro na tradução da mensagem do banco de dados.";
+            }
+        }
+    }
+}
